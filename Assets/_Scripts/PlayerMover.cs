3fb0714c205e2ed71715de3b7993dea0a398bf63
Yu <kgadbcksrb@gmail.com>
2024-02-28 10:43:56 +0900

    feat : 초기커밋

---
+++ Assets/_Scripts/PlayerMover.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] Animator animator;
+    [SerializeField] float moveSpeed;
+
+    Vector3 moveDir;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        controller.Move(moveDir * moveSpeed * Time.deltaTime);
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 input = value.Get<Vector2>();
+        moveDir.x = input.x;
+        moveDir.z = input.y;
+
+        animator.SetFloat("MoveSpeed", moveDir.magnitude);
+    }
+}
