ac4ec67123b201a1faa3a280591573a547706223
Yu <kgadbcksrb@gmail.com>
2024-02-29 18:02:01 +0900

    feat : 어댑터, 씬매니저 강의 끝

---
+++ Assets/_Scripts/Foothold.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foothold : MonoBehaviour
+{
+    public void Treaded()
+    {
+        Debug.Log("π‚¿Ω");
+    }
+}
---
+++ Assets/_Scripts/PlayerTreader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTreader : MonoBehaviour
+{
+    
+    
+
+    public void OnTriggerStay(Collider other)
+    {
+        
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
